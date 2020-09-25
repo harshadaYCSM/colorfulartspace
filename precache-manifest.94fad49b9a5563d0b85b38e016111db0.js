0bd7de7e4ceae88887d57ca18197887cb86b40ae
harshadaYCSM <charsh18@gmail.com>
2020-09-25 17:18:06 +0530

    Updates

---
+++ precache-manifest.94fad49b9a5563d0b85b38e016111db0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb51b10d58bf7382980025b6520d719",
+    "url": "/colorfulartspace/index.html"
+  },
+  {
+    "revision": "7d52e0ca3e0a34419b3b",
+    "url": "/colorfulartspace/static/css/main.f09e4dba.chunk.css"
+  },
+  {
+    "revision": "89904dda7cc99c4e1ddf",
+    "url": "/colorfulartspace/static/js/2.80913c74.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/colorfulartspace/static/js/2.80913c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d52e0ca3e0a34419b3b",
+    "url": "/colorfulartspace/static/js/main.316fce19.chunk.js"
+  },
+  {
+    "revision": "7d11571dae627ad54353",
+    "url": "/colorfulartspace/static/js/runtime-main.0d0fe509.js"
+  },
+  {
+    "revision": "d19870dbc839f2371c99d9a526d13b63",
+    "url": "/colorfulartspace/static/media/bg.d19870db.jpg"
+  }
+]);
